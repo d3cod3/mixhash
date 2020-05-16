69942049c31ba13012f02100a2be38568fbc4fa5
d3cod3 <n3m3da@d3cod3.org>
2020-05-16 07:10:13 +0200

    fixed neoscript for python3

--- mixhash/neoscrypt/module.c
+++ mixhash/neoscrypt/module.c
@@ -6,16 +6,28 @@ static PyObject *neoscrypt_getpowhash(PyObject *self, PyObject *args)
 {
     unsigned char *output;
     PyObject *value;
+#if PY_MAJOR_VERSION >= 3
+    PyBytesObject *input;
+#else
     PyStringObject *input;
+#endif
     if (!PyArg_ParseTuple(args, "S", &input))
         return NULL;
     Py_INCREF(input);
     output = PyMem_Malloc(32);
 
+#if PY_MAJOR_VERSION >= 3
+    neoscrypt((unsigned char *)PyBytes_AsString((PyObject*) input), output);
+#else
     neoscrypt((unsigned char *)PyString_AsString((PyObject*) input), output);
+#endif
 
     Py_DECREF(input);
+#if PY_MAJOR_VERSION >= 3
+    value = Py_BuildValue("y#", output, 32);
+#else
     value = Py_BuildValue("s#", output, 32);
+#endif
     PyMem_Free(output);
     return value;
 }
